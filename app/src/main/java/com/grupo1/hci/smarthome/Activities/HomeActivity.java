e933d42e96e57baeda873ab8e3724faa4606bd47
Santiago Swinnen <sswinnen@itba.edu.ar>
2017-11-16 02:16:35 -0300

    Agregado del GET a la API con volley y Gson. Manifest y Gradle modificados para poder acceder a internet y usar las herrmientas

--- app/src/main/java/com/grupo1/hci/smarthome/Activities/HomeActivity.java
+++ app/src/main/java/com/grupo1/hci/smarthome/Activities/HomeActivity.java
@@ -1,35 +1,44 @@
 package com.grupo1.hci.smarthome.Activities;
 
-import android.app.Activity;
-import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.ActionMode;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.grupo1.hci.smarthome.Model.Constants;
 import com.grupo1.hci.smarthome.Model.Room;
 import com.grupo1.hci.smarthome.R;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class HomeActivity extends NavigationActivity {
@@ -47,12 +56,14 @@ public class HomeActivity extends NavigationActivity {
     }
 
     ActionMode.Callback mActionModeCallback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setView();
         setOnClickListener();
+        getRoomsFromAPI();
 
     }
 
@@ -106,13 +117,10 @@ public class HomeActivity extends NavigationActivity {
     }
 
 
-
-
-    public void deleteRoom(String roomId)
-    {
+    public void deleteRoom(String roomId) {
         //setting the snackbar
         mySnackbar = Snackbar.make(findViewById(R.id.contentRoom_ListView), "Deleted", Snackbar.LENGTH_LONG);
-        mySnackbar.setAction("Undo", new View.OnClickListener(){
+        mySnackbar.setAction("Undo", new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -121,7 +129,7 @@ public class HomeActivity extends NavigationActivity {
         });
 
         mySnackbar.show();
-        deleteCountDown = new CountDownTimer(4000,1000) {
+        deleteCountDown = new CountDownTimer(4000, 1000) {
             @Override
             public void onTick(long l) {
 
@@ -137,15 +145,58 @@ public class HomeActivity extends NavigationActivity {
     public void selectedElement(View view, Room room) {
         view.setBackgroundColor(Color.GRAY);
         view.findViewById(R.id.rowLayout_iconImageView).setBackground(null);
-        selectedElement =view;
+        selectedElement = view;
         toolbar.setTitle(room.getName());
     }
 
     public void diselectElement() {
-        if(selectedElement != null){
+        if (selectedElement != null) {
             selectedElement.setBackgroundColor(Color.TRANSPARENT);
             toolbar.setTitle(Constants.appName);
         }
+    }
+
+
+    /**
+     * @author sswinnen
+     * Consulta los rooms a la API y a partir de lo que obtiene construye el ArrayAdapter de todos
+     * los rooms. Falta hacer bien la construccion, la estrucutura del llamado a la API creo que funciona
+     * Por ahora cuando la llamo no esta haciendo nada pero no tira error.
+     */
+    private void getRoomsFromAPI() {
+        RequestQueue mRequestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        mRequestQueue = new RequestQueue(cache, network);
+        mRequestQueue.start();
+
+        String url = "http://127.0.0.1:8080/api/rooms";
+        final Context c = this;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new Gson();
+                        Type listType = new TypeToken<ArrayList<Room>>() {}.getType();
+                        ArrayList<Room> roomList = gson.fromJson(response, listType);
+                        getRoomsFromAPI();
+
+                        ListView listView = (ListView) findViewById(R.id.contentRoom_ListView);
+                        if (listView != null) {
+                            ArrayAdapter arrayAdapter = new HomeAdapter(c,roomList);
+                            listView.setAdapter(arrayAdapter);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        mRequestQueue.add(stringRequest);
+
+        mRequestQueue.start();
     }
 
 }
